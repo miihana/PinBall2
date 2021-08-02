495e76696c71d691b72b866039f3acdb1fd38c90
miihana <mii0211sasa@gmail.com>
2021-08-02 23:32:11 +0900

    test
    test

---
+++ Assets/BallController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //スコア加算用
+    int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+  
+        
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        //デバック表示用
+        Debug.Log("衝突" + other.gameObject.tag);
+
+        // タグによってスコアを変える
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 15;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 30;
+        }
+
+        //ScoreTextにスコアを表示
+        this.scoreText.GetComponent<Text>().text = "Score:" + score;
+    }
+}
